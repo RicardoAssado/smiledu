3ae3325badb2fba37e1864ad884da70a030a2f64
Ricardo Assado <ricardo.assado.16@gmail.com>
2021-09-21 11:11:25 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,8 +1,17 @@
-/* === More plugins === */
+
 $(document).ready(function(){
 	/* === Menu Active === */
 	$('#menu').click(function(){
 		$('.main .content').toggleClass('show');
 	});
 
+	/* === Accordion === */
+	$(function () {
+		$(".accordion-item .accordion-content").css("display", "none");
+
+		$(".js-accordion").click(function () {
+			$(".open").not(this).removeClass("open").next().slideUp(400);
+			$(this).toggleClass("open").next().slideToggle(400);
+		});
+	});
 });
